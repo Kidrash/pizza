073becf227e5caf8b0d4158767dfa6f42022d0a8
Kidrash <markkaranja11@gmail.com>
2020-03-02 14:40:10 +0300

    chechbox added

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,33 @@
 $(Document).ready(function(){
-    
+  $('.Crustp').click(function(){
+    $('.crustp').hide();
+    $('#crust').toogle();
+    $('#crust').show();
+  })
+
+  $("#crust").click(function(){
+    $("#crust").hide();
+    $(".crustp").toggle();
+    $(".crustp").show();
+  })
+
+  $(".topsp").click(function(){
+    $("#tops").toggle();
+    $(".topsp").hide();
+    $("#tops").show();
+  })
+
+ $("#tops").click(function(){
+    $("#tops").hide();
+    $(".topsp").toggle();
+    $(".topsp").show();
+  })
+
+  $('.hov').hover(function () {
+    $(this).animate({opacity:'0.3'});
+    },
+    function () {
+    $(this).animate({opacity:'0'});
+});
+
 })
